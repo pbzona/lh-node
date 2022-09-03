231d9be6109f98979c121ff04340e7d2ab132bfb
Phil Zona <pzona@launchdarkly.com>
2022-09-02 21:24:57 -0400

    More features for progress tracker

--- src/progressTracker.js
+++ src/progressTracker.js
@@ -5,20 +5,39 @@ class ProgressTracker {
   constructor() {
     if (ProgressTracker.instance !== null) {
       ProgressTracker.instance = this;
-      this.completedModules = 0;
-      this.totalModules = moduleLoader.getNumberOfModules(); 
+      this.totalModules = moduleLoader.getNumberOfModules();
+
+      // Array of bools indicating which modules are complete
+      // e.g. [true, false, false, false] would mean the first of 4 modules is done
+      this.moduleStatus = Array(moduleLoader.getNumberOfModules()).fill(false);
     }
+    return ProgressTracker.instance;
   }
 
-  // Indicate that a new module has been completed by incrementing a count
-  increment() {
-    this.completedModules += 1;
+  // Returns number of modules completed
+  getCompletedModuleCount() {
+    return this.moduleStatus.reduce((prev, current) => {
+      if (current) return prev + 1;
+      return prev;
+    }, 0);
   }
 
   // Returns floating point value indicating the ratio of complete to total modules
   // Not sure if I'll end up using this
   getPercentageComplete() {
-    return (this.completedModules / this.totalModules).toFixed(2);
+    return (this.getCompletedModuleCount() / this.totalModules).toFixed(2);
+  }
+
+  // Set the completion status
+  setModuleStatus(index, val) {
+    const idx = index - 1; // Subctracting 1 so the index argument matches with the module's name
+    this.moduleStatus[idx] = !!val; // Ensure val is a boolean
+  }
+
+  // Returns whether a specific module (by number, not array index) is complete
+  isModuleComplete(index) {
+    const idx = index - 1;
+    return this.moduleStatus[idx];
   }
 }
 
