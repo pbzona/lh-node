dc720835b548575f131e3389f5475b1ab393b33d
Phil Zona <pzona@launchdarkly.com>
2022-10-10 13:57:24 -0400

    Replit conventions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./src/app');
 const { listenHandler } = require('./src/handlers');
 
-app.listen(process.env.PORT || 3001, listenHandler);
+app.listen(process.env.PORT || 3001, '0.0.0.0', listenHandler);
